f0160b9bb62a2365cf5952f81c3fb8cb1e973840
Alex Donskov <donskov_a@icloud.com>
2025-06-24 15:32:32 +0500

    add index

--- migrations/3_table_configuration.sql
+++ migrations/3_table_configuration.sql
@@ -1,6 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS configuration (
+    id SERIAL PRIMARY KEY,
     organization_id SERIAL REFERENCES organizations(id),
     new_client BOOLEAN DEFAULT FALSE,
     loan_available BOOLEAN DEFAULT FALSE,
@@ -8,11 +9,14 @@ CREATE TABLE IF NOT EXISTS configuration (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NULL,
     is_avaliable BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (organization_id)
 );
+CREATE INDEX IF NOT EXISTS idx_org_id ON configuration(organization_id);
+CREATE INDEX IF NOT EXISTS idx_is_avaliable ON configuration(is_avaliable);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS idx_org_id;
+DROP INDEX IF EXISTS idx_is_avaliable;
 DROP TABLE IF EXISTS configuration;
 -- +goose StatementEnd
